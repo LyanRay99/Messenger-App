a6a53c2df76f4b233f386d011578bffaceb5474b
Tai Marce <mttai161099@gmail.com>
2023-03-27 09:49:14 +0700

    init project

---
+++ Backend/Readme.md
@@ -0,0 +1,9 @@
+# Run project
+
+- Build & run project
+
+`yarn start`
+
+- Build & run project with nodemon
+
+`yarn run dev` or `nodemon`
---
+++ Backend/src/server.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from 'express'
+import path from 'path'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app: Express = express()
+
+//* convert request & response => json
+app.use(express.json())
+
+//* set static file
+//* trỏ đường dẫn từ file server.js (__dirname) => folder Public
+const publicPath = path.join(__dirname, './Public')
+app.use('/Public', express.static(publicPath))
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("Hello World! I'm Lyan Ray")
+})
+
+const PORT: string | number = process.env.DATABASE_PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
