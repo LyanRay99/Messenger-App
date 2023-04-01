7e89624800be9359943b6f26b7938c47eec9954d
Tai Marce <mttai161099@gmail.com>
2023-04-01 14:06:33 +0700

    init project (frontend)

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    // Phần này là cấu hình class container của tailwind lại theo các css property như bên dưới
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.container': {
+          maxWidth: theme('columns.7xl'),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.4'),
+          paddingRight: theme('spacing.4')
+        }
+      })
+    })
+    // require('@tailwindcss/line-clamp')
+  ]
+}
